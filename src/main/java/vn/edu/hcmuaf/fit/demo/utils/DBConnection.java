2fc788da4a932e23721a385d1b3527872cd3209e
PhanThiHuynhNhu <23130224@st.hcmuaf.edu.vn>
2026-04-14 15:24:49 +0700

    Fix bug

--- src/main/java/vn/edu/hcmuaf/fit/demo/utils/DBConnection.java
+++ src/main/java/vn/edu/hcmuaf/fit/demo/utils/DBConnection.java
@@ -1,4 +1,18 @@
 package vn.edu.hcmuaf.fit.demo.utils;
-
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 public class DBConnection {
+    private static final String URL = "jdbc:mysql://mysql:3306/dulieubanhang?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String USER = "root";
+    private static final String PASS = "12345";
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver MySQL không tìm thấy", e);
+        }
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
 }
--- src/main/java/vn/edu/hcmuaf/fit/demo/utils/PasswordUtil.java
+++ src/main/java/vn/edu/hcmuaf/fit/demo/utils/PasswordUtil.java
@@ -1,7 +1,11 @@
 package vn.edu.hcmuaf.fit.demo.utils;
-
+import org.mindrot.jbcrypt.BCrypt;
 public class PasswordUtil {
-    public static String hash(String s) {
-        return s;
+    public static String hash(String raw) {
+        return BCrypt.hashpw(raw, BCrypt.gensalt());
+    }
+
+    public static boolean verify(String raw, String hashed) {
+        return BCrypt.checkpw(raw, hashed);
     }
 }
